c4aceaa522e6b55eef8aed022304dd28463fdc23
Srinivas Kaza <srinivas@kaza.io>
2017-05-27 19:30:07 -0400

    Changes loop order to improve caching

--- src/simple_renderer.cpp
+++ src/simple_renderer.cpp
@@ -9,8 +9,8 @@ void SDFRenderer::operator()(uint32_t* bytes,
                              const int& pitch,
                              const int& width,
                              const int& height) {
-    for (int i = 0; i < width; i++) {
-        for (int j = 0; j < height; j++) {
+    for (int j = 0; j < height; j++) {
+        for (int i = 0; i < width; i++) {
             int offset = j * (pitch / sizeof(uint32_t)) + i;
             if (i % 30 < 5 || j % 30 < 5)
                 bytes[offset] = (255 << (16)) | (255 << (8)) | 200;
