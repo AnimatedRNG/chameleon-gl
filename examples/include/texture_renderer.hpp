5ecf7400e7624fb4ef1d12152b8e762060370812
Srinivas Kaza <srinivas@kaza.io>
2017-07-16 13:14:09 -0400

    Removes unnecessary content from TextureRenderer

--- examples/include/texture_renderer.hpp
+++ examples/include/texture_renderer.hpp
@@ -58,12 +58,9 @@ class TextureRenderer : public Renderer {
 
     virtual void operator()(const int& width,
                             const int& height) override {
-        glm::vec4 viewport(0, 0, width, height);
-
         program.bind();
         texture.bind();
         program.set_uniform("tex", texture);
-        program.set_uniform("viewport", viewport);
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
