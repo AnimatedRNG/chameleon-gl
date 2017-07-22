851af4cad7a484c6bf0962eeeeba02b801d332da
Srinivas Kaza <srinivas@kaza.io>
2017-07-22 17:58:45 -0400

    Updates texture shader to the new chml_viewport

--- shaders/texture_shader.fs
+++ shaders/texture_shader.fs
@@ -22,10 +22,10 @@
 out vec3 color;
 
 uniform sampler2D tex;
-uniform vec4 viewport;
+uniform vec4 chml_viewport;
 
 void main() {
-     vec2 position = gl_FragCoord.xy / viewport.zw;
+     vec2 position = gl_FragCoord.xy / chml_viewport.zw;
      position.y = 1.0 - position.y;
      color = texture(tex, position).rgb;
 }
